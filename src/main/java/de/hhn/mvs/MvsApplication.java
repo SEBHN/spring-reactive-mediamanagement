d9f6953ac8b5084ae1295866b9443ff2b4a12bdf
Manuel <wtfjoke@livenet.ch>
2018-10-11 11:35:50 +0200

    initial commit (spring initializr)

---
+++ src/main/java/de/hhn/mvs/MvsApplication.java
@@ -0,0 +1,12 @@
+package de.hhn.mvs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvsApplication.class, args);
+	}
+}
