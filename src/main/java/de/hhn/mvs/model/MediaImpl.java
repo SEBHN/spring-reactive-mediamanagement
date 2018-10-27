b7374332b83f2a1220b1a4442eea7b42db05bc23
Manuel <wtfjoke@livenet.ch>
2018-10-27 23:32:24 +0200

    add model definition of media

---
+++ src/main/java/de/hhn/mvs/model/MediaImpl.java
@@ -0,0 +1,9 @@
+package de.hhn.mvs.model;
+
+public class MediaImpl implements Media{
+    private String id;
+    private String name;
+    private String file;
+    private String fileExtension;
+    private String filePath;
+}
