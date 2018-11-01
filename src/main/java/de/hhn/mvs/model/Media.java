7e50c229d8feb8e06a3f0adde1af267d399573e6
Manuel <wtfjoke@livenet.ch>
2018-11-01 15:37:32 +0100

    fixes #33

--- src/main/java/de/hhn/mvs/model/Media.java
+++ src/main/java/de/hhn/mvs/model/Media.java
@@ -1,7 +1,36 @@
 package de.hhn.mvs.model;
 
+import java.util.List;
+
 /**
  * Media-metadata
  */
 public interface Media {
+
+    String getId();
+
+    String getName();
+
+    void setName(String name);
+
+    String getFile();
+
+    void setFile(String file);
+
+    String getFileExtension();
+
+    void setFileExtension(String fileExtension);
+
+    String getFilePath();
+
+    void setFilePath(String filePath);
+
+    List<Tag> getTags();
+
+    void setTags(List<Tag> tags);
+
+    boolean addTag(Tag tag);
+
+    boolean removeTag(Tag tag);
+
 }
--- src/main/java/de/hhn/mvs/model/MediaImpl.java
+++ src/main/java/de/hhn/mvs/model/MediaImpl.java
@@ -1,9 +1,87 @@
 package de.hhn.mvs.model;
 
-public class MediaImpl implements Media{
+import java.util.Arrays;
+import java.util.List;
+
+public final class MediaImpl implements Media {
     private String id;
     private String name;
     private String file;
     private String fileExtension;
     private String filePath;
+    private List<Tag> tags;
+
+    public MediaImpl(String id, String name, String file, String fileExtension, String filePath, Tag... tags) {
+        this.id = id;
+        this.name = name;
+        this.file = file;
+        this.fileExtension = fileExtension;
+        this.filePath = filePath;
+        this.tags = Arrays.asList(tags);
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getFile() {
+        return file;
+    }
+
+    @Override
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+    @Override
+    public String getFileExtension() {
+        return fileExtension;
+    }
+
+    @Override
+    public void setFileExtension(String fileExtension) {
+        this.fileExtension = fileExtension;
+    }
+
+    @Override
+    public String getFilePath() {
+        return filePath;
+    }
+
+    @Override
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    @Override
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    @Override
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+
+    @Override
+    public boolean addTag(Tag tag) {
+        return this.tags.add(tag);
+    }
+
+    @Override
+    public boolean removeTag(Tag tag) {
+        return this.tags.remove(tag);
+    }
 }
