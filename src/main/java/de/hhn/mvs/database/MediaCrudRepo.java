d463e3e46dc36ac0b4869d8706a88a9689a01cd0
Johanna Mergler <jmergler@stud.hs-heilbronn.de>
2018-11-06 18:16:27 +0100

    introduced reactive mongodb media crud interface

---
+++ src/main/java/de/hhn/mvs/database/MediaCrudRepo.java
@@ -0,0 +1,30 @@
+package de.hhn.mvs.database;
+
+import de.hhn.mvs.model.Media;
+import de.hhn.mvs.model.MediaImpl;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+
+import java.io.File;
+
+@Repository
+public interface MediaCrudRepo
+        extends ReactiveCrudRepository<MediaImpl, String> {
+
+//    public void saveFile(File file, String mediaId);
+
+//    Mono<MediaImpl> saveMedia();
+    //public Mono<String> saveMedia(MediaImpl media);
+
+    Mono<MediaImpl> findMediaImplById (String id);
+
+    
+
+
+
+
+}
--- src/main/java/de/hhn/mvs/model/MediaImpl.java
+++ src/main/java/de/hhn/mvs/model/MediaImpl.java
@@ -1,6 +1,11 @@
 package de.hhn.mvs.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class MediaImpl implements Media{
+    @Id
     private String id;
     private String name;
     private String file;
