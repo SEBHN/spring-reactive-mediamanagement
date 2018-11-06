7722a281ee77c227bbad694c007ad7278c27ab04
Manuel <wtfjoke@livenet.ch>
2018-11-07 00:48:59 +0100

    implement reactive upload of file

--- src/main/java/de/hhn/mvs/rest/MediaRouter.java
+++ src/main/java/de/hhn/mvs/rest/MediaRouter.java
@@ -5,8 +5,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.http.MediaType.APPLICATION_OCTET_STREAM;
+import static org.springframework.http.MediaType.*;
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
 @Component
@@ -18,6 +17,7 @@ public class MediaRouter {
                 .route(GET("/users/{userId}/media/{id}").and(accept(APPLICATION_JSON)), mediaHandler::get)
                 .andRoute(GET("/users/{userId}/media/{id}/download").and(accept(APPLICATION_OCTET_STREAM)), mediaHandler::download)
                 .andRoute(GET("/users/{userId}/media").and(accept(APPLICATION_JSON)), mediaHandler::list)
-                .andRoute(POST("/users/{userId}/media"), mediaHandler::create);
+                .andRoute(POST("/users/{userId}/media"), mediaHandler::create)
+                .andRoute(POST("/users/{userId}/media/{id}/upload").and(accept(MULTIPART_FORM_DATA)), mediaHandler::upload);
     }
 }
