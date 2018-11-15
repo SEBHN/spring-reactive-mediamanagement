a962e79ab147a958fc2a35449a312ae45cbe49a4
Manuel <wtfjoke@livenet.ch>
2018-11-15 14:35:41 +0100

    add new query for finding media with id and userid
    delete unneeded methods

--- src/main/java/de/hhn/mvs/database/MediaCrudRepo.java
+++ src/main/java/de/hhn/mvs/database/MediaCrudRepo.java
@@ -1,6 +1,7 @@
 package de.hhn.mvs.database;
 
 import de.hhn.mvs.model.Media;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -8,6 +9,11 @@ import reactor.core.publisher.Mono;
 
 @Repository
 public interface MediaCrudRepo extends ReactiveMongoRepository<Media, String> {
-    Mono<Media> save(Media media);
-    Flux<Media> findAll();
+
+    @Query("{ 'id': ?0, 'ownerId': ?1}")
+    Mono<Media> findByIdAndUser(String id, String userId);
+
+    @Query("{ 'ownerId': ?0}")
+    Flux<Media> findByUser(String userId);
+
 }
