b436e92476b09b0562d24d091911e9e8a375ed4a
mberg <mberg@stud.hs-heilbronn.de>
2018-11-22 16:07:27 +0100

    added User-Handler and Router

---
+++ src/main/java/de/hhn/mvs/rest/UserRouter.java
@@ -0,0 +1,22 @@
+package de.hhn.mvs.rest;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+
+import static org.springframework.http.MediaType.*;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+
+@Component
+public class UserRouter {
+    @Bean
+    public RouterFunction route(UserHandler userHandler) {
+        return RouterFunctions
+                .route(GET("/users/{userId}").and(accept(APPLICATION_JSON)), userHandler::get)
+                .andRoute(POST("/users").and(accept(APPLICATION_JSON)), userHandler::create)
+                .filter(userHandler.illegalStateToBadRequest())
+                .andRoute(PUT("/users/{userId}").and(accept(APPLICATION_JSON)), userHandler::update)
+                .andRoute(DELETE("/users/{userId}").and(accept(ALL)), userHandler::delete);
+    }
+}
