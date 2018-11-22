a2fa2b9e64d19978423cc4b8fb791433785798da
mberg <mberg@stud.hs-heilbronn.de>
2018-11-22 15:39:05 +0100

    added User-INterface and User

---
+++ src/main/java/de/hhn/mvs/model/UserImpl.java
@@ -0,0 +1,67 @@
+package de.hhn.mvs.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public final class UserImpl implements User{
+    @Id
+    private String id;
+    private boolean admin;
+    private String email;
+    private String password;
+    private String token;
+
+    public UserImpl(){
+
+    }
+
+    public UserImpl(String id, boolean admin, String email, String password, String token){
+        this.id = id;
+        this.admin = admin;
+        this.email = email;
+        this.password = password;
+        this.token = token;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String getToken() {
+        return token;
+    }
+
+    @Override
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+}
