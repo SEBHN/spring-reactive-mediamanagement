a87121100a43923c13461faf04394db6b2001d52
mberg <mberg@stud.hs-heilbronn.de>
2018-11-27 11:52:20 +0100

    Implemented equa, hash and toString in UserImpl

--- src/main/java/de/hhn/mvs/model/UserImpl.java
+++ src/main/java/de/hhn/mvs/model/UserImpl.java
@@ -3,6 +3,8 @@ package de.hhn.mvs.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Objects;
+
 @Document
 public final class UserImpl implements User{
     @Id
@@ -62,6 +64,34 @@ public final class UserImpl implements User{
     @Override
     public void setToken(String token) {
         this.token = token;
+    }
+
+    @Override
+    public String toString(){
+        return "User{" +
+                "id='" + id + "\'" +
+                "admin='" + admin + "\'" +
+                "email='" + email + "\'" +
+                "password='" + password + "\'" +
+                "token='" + token + "\'" +
+                "}";
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserImpl user = (UserImpl) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(admin, user.admin) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(password, user.password) &&
+                Objects.equals(token, user.token);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(id, admin, email, password, token);
     }
 
 }
--- src/test/java/de/hhn/mvs/rest/UserHandlerTest.java
+++ src/test/java/de/hhn/mvs/rest/UserHandlerTest.java
@@ -6,13 +6,20 @@ import de.hhn.mvs.model.UserImpl;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureWebTestClient
 public class UserHandlerTest {
 
     @Autowired
