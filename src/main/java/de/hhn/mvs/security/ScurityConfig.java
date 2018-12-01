d43e98e7cc13ab6c01ea62973253b5f5229cbe8a
Stefan Kadlubski <stefan.kadlubski@gmail.com>
2018-12-01 14:03:21 +0100

    security v0.1 #nobuild

---
+++ src/main/java/de/hhn/mvs/security/ScurityConfig.java
@@ -0,0 +1,137 @@
+package de.hhn.mvs.auth;
+
+import de.hhn.mvs.auth.basic.BasicAuthenticationSuccessHandler;
+import de.hhn.mvs.auth.bearer.BearerTokenReactiveAuthenticationManager;
+import de.hhn.mvs.auth.bearer.ServerHttpBearerAuthenticationConverter;
+import de.hhn.mvs.database.UserCrudRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
+import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
+import org.springframework.security.web.server.authorization.AuthorizationContext;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
+@EnableWebFluxSecurity
+public class ScurityConfig {
+
+
+    UserCrudRepo users;
+    /**
+     * A custom UserDetailsService to provide quick user rights for Spring Security,
+     * more formal implementations may be added as separated files and annotated as
+     * a Spring stereotype.
+     *
+     * @return MapReactiveUserDetailsService an InMemory implementation of user details
+     */
+    @Bean
+    public MapReactiveUserDetailsService userDetailsRepository() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user")
+                .roles("USER", "ADMIN")
+                .build();
+        return new MapReactiveUserDetailsService(user);
+    }
+
+    /**
+     * For Spring Security webflux, a chain of filters will provide user authentication
+     * and authorization, we add custom filters to enable JWT token approach.
+     *
+     * @param http An initial object to build common filter scenarios.
+     *             Customized filters are added here.
+     * @return SecurityWebFilterChain A filter chain for web exchanges that will
+     * provide security
+     **/
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+
+        http
+            .authorizeExchange()
+            .pathMatchers("/login", "/")
+            .authenticated()
+            .and()
+            .addFilterAt(basicAuthenticationFilter(), SecurityWebFiltersOrder.HTTP_BASIC)
+
+            .authorizeExchange()
+            .pathMatchers("/api/**")
+            .authenticated()
+            .and()
+            .addFilterAt(bearerAuthenticationFilter(), SecurityWebFiltersOrder.AUTHENTICATION);
+        return http.build();
+    }
+
+    private Mono<AuthorizationDecision> currentUserMatchesPath(Mono<Authentication> authentication, AuthorizationContext context) {
+        return authentication
+                .map(a -> context.getVariables().get("user").equals(a.getName()))
+                .map(granted -> new AuthorizationDecision(granted));
+    }
+
+    @Bean
+    public ReactiveUserDetailsService userDetailsService(UserCrudRepo users) {
+        return (username) -> users.findByUsername(username).cast(UserDetails.class);
+    }
+
+
+    /**
+     * Use the already implemented logic in  AuthenticationWebFilter and set a custom
+     * SuccessHandler that will return a JWT when a user is authenticated with user/password
+     * Create an AuthenticationManager using the UserDetailsService defined above
+     *
+     * @return AuthenticationWebFilter
+     */
+    private AuthenticationWebFilter basicAuthenticationFilter(){
+        UserDetailsRepositoryReactiveAuthenticationManager authManager;
+        AuthenticationWebFilter basicAuthenticationFilter;
+        ServerAuthenticationSuccessHandler successHandler;
+
+        authManager = new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsRepository());
+        successHandler = new BasicAuthenticationSuccessHandler();
+
+        basicAuthenticationFilter = new AuthenticationWebFilter(authManager);
+        basicAuthenticationFilter.setAuthenticationSuccessHandler(successHandler);
+
+        return basicAuthenticationFilter;
+
+    }
+
+    /**
+     * Use the already implemented logic by AuthenticationWebFilter and set a custom
+     * converter that will handle requests containing a Bearer token inside
+     * the HTTP Authorization header.
+     * Set a dummy authentication manager to this filter, it's not needed because
+     * the converter handles this.
+     *
+     * @return bearerAuthenticationFilter that will authorize requests containing a JWT
+     */
+    private AuthenticationWebFilter bearerAuthenticationFilter(){
+        AuthenticationWebFilter bearerAuthenticationFilter;
+        Function<ServerWebExchange, Mono<Authentication>> bearerConverter;
+        ReactiveAuthenticationManager authManager;
+
+        authManager  = new BearerTokenReactiveAuthenticationManager();
+        bearerAuthenticationFilter = new AuthenticationWebFilter(authManager);
+        bearerConverter = new ServerHttpBearerAuthenticationConverter();
+
+        bearerAuthenticationFilter.setAuthenticationConverter(bearerConverter);
+        bearerAuthenticationFilter.setRequiresAuthenticationMatcher(ServerWebExchangeMatchers.pathMatchers("/api/**"));
+
+        return bearerAuthenticationFilter;
+    }
+
+}
