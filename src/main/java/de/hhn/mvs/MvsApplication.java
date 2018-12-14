30b89b6570e3d253b4c3ba937e64925ac8834c13
Manuel <wtfjoke@livenet.ch>
2018-12-14 23:34:47 +0100

    It somehow works!

--- src/main/java/de/hhn/mvs/MvsApplication.java
+++ src/main/java/de/hhn/mvs/MvsApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @EnableOAuth2Sso
 @SpringBootApplication
@@ -29,5 +32,15 @@ public class MvsApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(MvsApplication.class, args);
+    }
+
+    @GetMapping("/")
+    String home(Principal user) {
+        return "Hello " + user.getName();
+    }
+
+    @GetMapping("/login")
+    String bla() {
+        return "Hello ";
     }
 }
