a5c2cfef7443b7bca0f81840729cba9945302efd
Manuel <wtfjoke@livenet.ch>
2018-12-14 10:04:03 +0100

    Add javadoc

--- src/main/java/de/hhn/mvs/metadata/MetadataTranslator.java
+++ src/main/java/de/hhn/mvs/metadata/MetadataTranslator.java
@@ -2,10 +2,23 @@ package de.hhn.mvs.metadata;
 
 import java.util.Map;
 
+/**
+ * Im responsible for translating metadata keys and sometimes its values into a human readable format
+ */
 public interface MetadataTranslator {
 
+    /**
+     * Collecting the needed keys/values and translate them
+     * @param metadataKey - original metadata parsed key
+     * @param metadataValue - original metadata value
+     * @return this
+     */
     MetadataTranslator collect(String metadataKey, String metadataValue);
 
+    /**
+     * Returns the translated metadata which were collected with {@link #collect(String, String)}
+     * @return Map with all metadata
+     */
     Map<String, String> getMetadata();
 
 
