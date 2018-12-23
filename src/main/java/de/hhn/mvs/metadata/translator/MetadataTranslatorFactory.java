6bc1f6c462109a24d6c1714fe87759794f66d6a7
Manuel <wtfjoke@livenet.ch>
2018-12-23 15:37:21 +0100

    Add logger for error trace in gcloud app engine

--- src/main/java/de/hhn/mvs/metadata/translator/MetadataTranslatorFactory.java
+++ src/main/java/de/hhn/mvs/metadata/translator/MetadataTranslatorFactory.java
@@ -1,11 +1,15 @@
 package de.hhn.mvs.metadata.translator;
 
+import java.util.logging.Logger;
+
 import org.apache.tika.mime.MediaType;
 
 /**
  * Im responsible for getting you the right {@link MetadataTranslator}
  */
 public class MetadataTranslatorFactory {
+
+    private static final Logger logger = Logger.getLogger(MetadataTranslatorFactory.class.getName());
 
     /**
      * I will return you the {@link MetadataTranslator} for the passed {@link MediaType}
@@ -14,6 +18,7 @@ public class MetadataTranslatorFactory {
      */
     public static MetadataTranslator get(MediaType contentType) {
         if (contentType == null) {
+            logger.warning("contentType is null, returning fallback-translator");
             return new FallbackMetadataTranslator();
         }
         switch (contentType.getType()) {
@@ -24,6 +29,7 @@ public class MetadataTranslatorFactory {
             case "video":
                 return new VideoMetadataTranslator(contentType);
         }
+        logger.info("No Translator found, returning fallback");
         return new FallbackMetadataTranslator();
     }
 }
