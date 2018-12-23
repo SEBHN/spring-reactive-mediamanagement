e8d83a714dad1bc5e1574d70b847c5aaf138a9d8
Johanna Mergler <jmergler@stud.hs-heilbronn.de>
2018-12-23 19:08:40 +0100

    delete import and space

--- src/main/java/de/hhn/mvs/database/MediaCrudRepo.java
+++ src/main/java/de/hhn/mvs/database/MediaCrudRepo.java
@@ -1,11 +1,9 @@
 package de.hhn.mvs.database;
 
-import com.mongodb.client.result.DeleteResult;
 import de.hhn.mvs.model.Media;
 import de.hhn.mvs.model.Tag;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -23,6 +21,5 @@ public interface MediaCrudRepo extends ReactiveMongoRepository<Media, String> {
 
     @Query("{ 'ownerId': ?0, filePath: {$regex: ?1}, tags: { $all : ?2}}")
     Flux<Media> findAllByOwnerIdAndFilePathRegexAndTagsContainingAll_caseSensitive(String ownerId, String regex, List<Tag> tags);
-
 }
 
