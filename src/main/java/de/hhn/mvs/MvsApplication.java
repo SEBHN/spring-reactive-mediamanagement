4fbc4e43c1e1af0f1034232a194e274ae7ebc8b3
Manuel <wtfjoke@livenet.ch>
2018-12-27 23:23:32 +0100

    Use okta starter config and switch to implicit_grant
    move corsFilter into security configuration

--- src/main/java/de/hhn/mvs/MvsApplication.java
+++ src/main/java/de/hhn/mvs/MvsApplication.java
@@ -2,51 +2,11 @@ package de.hhn.mvs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
-
-@EnableOAuth2Sso
 @SpringBootApplication
-@RestController
-public class MvsApplication extends WebSecurityConfigurerAdapter {
-//public class MvsApplication {
-
-//later: check https
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.requiresChannel().anyRequest().requiresSecure();
-//    }
-
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login").permitAll();
-    }
-
+public class MvsApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(MvsApplication.class, args);
     }
-
-    @GetMapping("/")
-    String home(Principal user) {
-        return "Hello " + user.getName();
-    }
-
-    @GetMapping("/token")
-    String token(OAuth2Authentication user) {
-        OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) user.getDetails();
-        return details.getTokenValue();
-    }
-
 }
---
+++ src/main/java/de/hhn/mvs/SecurityConfiguration.java
@@ -0,0 +1,51 @@
+package de.hhn.mvs;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class SecurityConfiguration {
+
+    private static final String FRONTEND_LOCALHOST = "http://localhost:4200";
+    private static final String FRONTEND_STAGING = "https://sebhn.github.io";
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf()
+                .csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
+    }
+
+
+    @Bean
+    CorsConfigurationSource corsConfiguration() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.applyPermitDefaultValues();
+        corsConfig.addAllowedMethod(HttpMethod.PUT);
+        corsConfig.addAllowedMethod(HttpMethod.DELETE);
+        corsConfig.setAllowedOrigins(Arrays.asList(FRONTEND_LOCALHOST, FRONTEND_STAGING));
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
+    }
+}
